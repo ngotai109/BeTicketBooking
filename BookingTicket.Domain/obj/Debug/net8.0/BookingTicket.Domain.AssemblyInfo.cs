1e8a3eded77e41f70be324c95b29353b1e5b45af
ngotai109 <glucozo109@gmail.com>
2026-03-05 22:27:39 +0700

    fix

--- BookingTicket.Domain/obj/Debug/net8.0/BookingTicket.Domain.AssemblyInfo.cs
+++ BookingTicket.Domain/obj/Debug/net8.0/BookingTicket.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingTicket.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201c4e63a29104f6a4f85ec898a21bf0785d0894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a0236e4ef838bbee5a5aac0e9d831531d1079a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingTicket.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingTicket.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
