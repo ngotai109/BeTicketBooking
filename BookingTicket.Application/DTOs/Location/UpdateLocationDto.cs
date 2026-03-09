6e552b3d18ebfc9017a96c466427f60ccd071f7e
ngotai109 <glucozo109@gmail.com>
2026-03-09 11:32:27 +0700

    Restore deleted code files from commit 462e3f5

---
+++ BookingTicket.Application/DTOs/Location/UpdateLocationDto.cs
@@ -0,0 +1,7 @@
+namespace BookingTicket.Application.DTOs.Location
+{
+    public class UpdateLocationDto
+    {
+        public string LocationName { get; set; } = string.Empty;
+    }
+}
---
+++ BookingTicket.Application/Interfaces/IRouteRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookingTicket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingTicket.Application.Interfaces
+{
+    public interface IRouteRepository : IGenericRepository<Routes>
+    {
+        Task<bool> ExitsByAsyncName(string name);
+    }
+}
